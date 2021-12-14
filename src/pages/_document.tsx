14a10041790ebf855982fd180479cb4cb5c301e2
Leonardo <34871458+LeozinH1@users.noreply.github.com>
2021-12-14 01:27:44 -0300

    Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,8 @@ import Document, {
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
+import { GA_TRACKING_ID } from "../utils";
+
 export default class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
@@ -49,6 +51,23 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
             rel="stylesheet"
+          />
+
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+            }}
           />
         </Head>
         <body>
